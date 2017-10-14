19fe35dae27249d532c34fd73a6bbc2e3e8022cf
chocoluffy <luffy.yu@mail.utoronto.ca>
2017-10-14 17:25:31 -0400

    Add image

--- 4-LDA-On-Tfidf/run.py
+++ 4-LDA-On-Tfidf/run.py
@@ -129,265 +129,260 @@ else:
     corpus = corpora.MmCorpus('./models/new-doc-term.mm')
     print("document to term matrix loaded...")
 
+# Use TF-IDF model
 tfidf = gensim.models.TfidfModel(corpus, normalize=True)
 corpus_tfidf = tfidf[corpus]
 
-corpus_tfidf = map(lambda x: map(lambda y: (y[0], round(y[1] * 100, 2)), x), corpus_tfidf)
+# corpus_tfidf = map(lambda x: map(lambda y: (y[0], round(y[1] * 100, 2)), x), corpus_tfidf)
 
 # pprint(dictionary[237])
 # print("tfidf weights of the first document after de-normalization")
-print("BOW of the first document")
-pprint(map(lambda x: (dictionary[x[0]], x[1]), corpus[0]))
+# print("BOW of the first document")
+# pprint(map(lambda x: (dictionary[x[0]], x[1]), corpus[0]))
 # pprint(len(corpus[0]))
 
-# import logging
-# logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-# # Save LDA model
-# if not os.path.exists('./models/tfidf200.lda'):
-#     # Creating the object for LDA model using gensim library
-#     Lda = gensim.models.ldamodel.LdaModel
-#     # Running and Trainign LDA model on the document term matrix.
-#     ldamodel = Lda(corpus_tfidf, num_topics=200, id2word = dictionary, passes=60)
-#     ldamodel.save('./models/tfidf200.lda')
-# else:
-#     ldamodel = gensim.models.ldamodel.LdaModel.load('models/tfidf200.lda')
-#     print("lda model loaded...")
+# Save LDA model
+if not os.path.exists('./models/tfidf.lda'):
+    # Creating the object for LDA model using gensim library
+    Lda = gensim.models.ldamodel.LdaModel
+    # Running and Trainign LDA model on the document term matrix.
+    ldamodel = Lda(corpus, num_topics=100, id2word = dictionary, passes=50)
+    ldamodel.save('./models/tfidf.lda')
+else:
+    ldamodel = gensim.models.ldamodel.LdaModel.load('models/tfidf.lda')
+    print("lda model loaded...")
 
 
 
-# def print_general_subreddit_topic():
-#     """
-#     Print all 100 topics for human inspection.
-#     """
-#     ldamodel.print_topics(100)
+def print_general_subreddit_topic():
+    """
+    Print all 100 topics for human inspection.
+    """
+    ldamodel.print_topics(100)
 
 # print_general_subreddit_topic()
 
 
-# # # Return all topics probability distribution for each document, instead of clipping value using threshold.
-# # def get_doc_topics(lda, bow):
-# #     gamma, _ = lda.inference([bow])
-# #     topic_dist = gamma[0] / sum(gamma[0])
-# #     # return [(topic_id, topic_value) for topic_id, topic_value in enumerate(topic_dist)]
-# #     return topic_dist # return direct value
+# Return all topics probability distribution for each document, instead of clipping value using threshold.
+def get_doc_topics(lda, bow):
+    gamma, _ = lda.inference([bow])
+    topic_dist = gamma[0] / sum(gamma[0])
+    # return [(topic_id, topic_value) for topic_id, topic_value in enumerate(topic_dist)]
+    return topic_dist # return direct value
 
 
-# # # Covert the comments from data into a topic vector, for now given 100 topics, each vector will be 
-# # # of length 100, indicating the probability from each topic.
-# # rc_tvec = []
-# # for doc_term_vec in corpus:
-# #     topic_dist = get_doc_topics(ldamodel, doc_term_vec)
-# #     rc_tvec.append(topic_dist)
-# #     # topic_dist = ldamodel[doc_term_vec] # Bad, only shows top several topics; it clips the topic value under threshold.
+# Covert the comments from data into a topic vector, for now given 100 topics, each vector will be 
+# of length 100, indicating the probability from each topic.
+rc_tvec = []
+for doc_term_vec in corpus_tfidf:
+    topic_dist = get_doc_topics(ldamodel, doc_term_vec)
+    rc_tvec.append(topic_dist)
+    # topic_dist = ldamodel[doc_term_vec] # Bad, only shows top several topics; it clips the topic value under threshold.
     
 
 
-# # # Below are tasks utilizing the above model.
+# # Below are tasks utilizing the above model.
 
 
-# # # pprint(rc_tvec)
-# # # Print the topic-words distribution for some specific topics.
-# # import operator
-# # def print_specific_subreddit_topic():    
-# #     dominant_counter = 0
-# #     for label, topic_dist_vec in zip(subreddits, rc_tvec):
-# #         index, value = max(enumerate(topic_dist_vec), key=operator.itemgetter(1))
-# #         if label in ["cars", "guns", "DIY", "DotA2", "toronto", "cats", "food", "nba"]:
-# #             print "For subreddit: ", label
-# #             print "its topic distribution vector: ", topic_dist_vec
-# #             print "the dominant topic is: ", index, " with prob: ", value, ldamodel.show_topic(index, topn=20)
-# #         if value > 0.5:
-# #             dominant_counter += 1
-# #     print "From ", len(subreddits), " all subreddit, ", dominant_counter, " of all owns a dominant topics"
+# pprint(rc_tvec)
+# Print the topic-words distribution for some specific topics.
+import operator
+def print_specific_subreddit_topic():    
+    dominant_counter = 0
+    for label, topic_dist_vec in zip(subreddits, rc_tvec):
+        index, value = max(enumerate(topic_dist_vec), key=operator.itemgetter(1))
+        if label in ["cars", "guns", "DIY", "DotA2", "toronto", "cats", "food", "nba"]:
+            print "For subreddit: ", label
+            print "its topic distribution vector: ", topic_dist_vec
+            print "the dominant topic is: ", index, " with prob: ", value, ldamodel.show_topic(index, topn=20)
+        if value > 0.5:
+            dominant_counter += 1
+    print "From ", len(subreddits), " all subreddit, ", dominant_counter, " of all owns a dominant topics"
 
-# # # print_specific_subreddit_topic()
-
-# # def print_general_subreddit_topic():
-# #     """
-# #     Randomly print out 20 topics for human inspection.
-# #     """
-# #     ldamodel.print_topics(20)
+# print_specific_subreddit_topic()
 
 
 
-# # def find_dom_topic_vec(name):
-# #     """
-# #     Assume a subreddit can be represented by its most dominant topic vector. (currently ignore less dominant topics, 
-# #     as one subreddit can cover several topics). 
-# #     if name not appear in the subreddits list(all subreddit label from data), return -1.
-# #     """
-# #     if name not in subreddits:
-# #         print("subreddit not in list...")
-# #         return -1
-# #     else:
-# #         subreddits_index = subreddits.index(name)
-# #         topic_vec = rc_tvec[subreddits_index]
-# #         dom_topic_index, prob = max(enumerate(topic_vec), key=operator.itemgetter(1))
-# #         return dom_topic_index
+# def find_dom_topic_vec(name):
+#     """
+#     Assume a subreddit can be represented by its most dominant topic vector. (currently ignore less dominant topics, 
+#     as one subreddit can cover several topics). 
+#     if name not appear in the subreddits list(all subreddit label from data), return -1.
+#     """
+#     if name not in subreddits:
+#         print("subreddit not in list...")
+#         return -1
+#     else:
+#         subreddits_index = subreddits.index(name)
+#         topic_vec = rc_tvec[subreddits_index]
+#         dom_topic_index, prob = max(enumerate(topic_vec), key=operator.itemgetter(1))
+#         return dom_topic_index
 
 
-# # def load_author_from_mongo():
-# #     # use pymongo to load large chunk of data from mongo.
-# #     if not os.path.exists('./models/author_topics.pkl'):
-# #         # Use pymongo to achieve same effect as below codes. 
+# def load_author_from_mongo():
+#     # use pymongo to load large chunk of data from mongo.
+#     if not os.path.exists('./models/author_topics.pkl'):
+#         # Use pymongo to achieve same effect as below codes. 
 
-# #         pipe = [
-# #             {'$group': {
-# #                 '_id': '$author',
-# #                 'contributions': { '$push':  { 'subreddit': "$subreddit", 'ups': "$ups" } },
-# #                 'subredditset': {'$addToSet': "$subreddit"}
-# #             }},
-# #             {'$addFields': { 'subredditnum': { '$size': "$subredditset" } } },
-# #             { "$project": { 
-# #                 "subredditset": 0
-# #             }},
-# #             {'$match': {'subredditnum': {'$gt': 5}} }
-# #         ]
+#         pipe = [
+#             {'$group': {
+#                 '_id': '$author',
+#                 'contributions': { '$push':  { 'subreddit': "$subreddit", 'ups': "$ups" } },
+#                 'subredditset': {'$addToSet': "$subreddit"}
+#             }},
+#             {'$addFields': { 'subredditnum': { '$size': "$subredditset" } } },
+#             { "$project": { 
+#                 "subredditset": 0
+#             }},
+#             {'$match': {'subredditnum': {'$gt': 5}} }
+#         ]
 
-# #         data = defaultdict(dict)
-# #         counter = 0
-# #         for document in db.docs_l2.aggregate(pipeline = pipe, allowDiskUse = True):
-# #             counter += 1
-# #             print "Processing #%d author"%(counter)
-# #             reddit_ups_data = defaultdict(int)
-# #             for reddit in document['contributions']:
-# #                 reddit_ups_data[reddit['subreddit']] += 1 # use reddit["ups"] or simply 1?
-# #             reddit_dom_topic_vec = defaultdict(dict)
-# #             for name in reddit_ups_data.iterkeys():
-# #                 reddit_dom_topic_vec[name] = find_dom_topic_vec(name)
+#         data = defaultdict(dict)
+#         counter = 0
+#         for document in db.docs_l2.aggregate(pipeline = pipe, allowDiskUse = True):
+#             counter += 1
+#             print "Processing #%d author"%(counter)
+#             reddit_ups_data = defaultdict(int)
+#             for reddit in document['contributions']:
+#                 reddit_ups_data[reddit['subreddit']] += 1 # use reddit["ups"] or simply 1?
+#             reddit_dom_topic_vec = defaultdict(dict)
+#             for name in reddit_ups_data.iterkeys():
+#                 reddit_dom_topic_vec[name] = find_dom_topic_vec(name)
             
-# #             data[document['_id']]['contributions'] = reddit_ups_data
-# #             data[document['_id']]['topicvecs'] = reddit_dom_topic_vec
-# #             # print(data)
+#             data[document['_id']]['contributions'] = reddit_ups_data
+#             data[document['_id']]['topicvecs'] = reddit_dom_topic_vec
+#             # print(data)
 
 
-# #         pickle.dump(data, open("./models/author_topics.pkl", 'wb'))
-# #         return data
-# #     else:
-# #         data = pickle.load(open("./models/author_topics.pkl", 'rb'))
-# #         print("author topics model loaded...")
-# #         return data
+#         pickle.dump(data, open("./models/author_topics.pkl", 'wb'))
+#         return data
+#     else:
+#         data = pickle.load(open("./models/author_topics.pkl", 'rb'))
+#         print("author topics model loaded...")
+#         return data
 
 
 # # author_topics = load_author_from_mongo()
 
 
-# # # """
-# # # Below are calculating pairwise topic similarity.
-# # # """
+# """
+# Below are calculating pairwise topic similarity.
+# """
 
-# # import numpy as np
-# # from scipy.spatial.distance import pdist, squareform
-# # import itertools
+# import numpy as np
+# from scipy.spatial.distance import pdist, squareform
+# import itertools
 
-# # def hellinger(X):
-# #     return squareform(pdist(np.sqrt(X)))/np.sqrt(2)
+# def hellinger(X):
+#     return squareform(pdist(np.sqrt(X)))/np.sqrt(2)
 
-# # X = ldamodel.state.get_lambda()
-# # X = X / X.sum(axis=1)[:, np.newaxis] # normalize vector
-# # h = hellinger(X)
+# X = ldamodel.state.get_lambda()
+# X = X / X.sum(axis=1)[:, np.newaxis] # normalize vector
+# h = hellinger(X)
 
-# # # book = dict(zip(subreddits, range(len(subreddits)))) # a dictionary map subreddit name to its 
+# # book = dict(zip(subreddits, range(len(subreddits)))) # a dictionary map subreddit name to its 
 
-# # for author, obj in author_topics.items():
-# #     """
-# #     First filter by weight cutoff T = 4, remove subreddit contributions less than T.
-# #     """
-# #     freq_reddit = []
-# #     for name in obj['contributions'].iterkeys():
-# #         if obj['contributions'][name] >= 4:
-# #             freq_reddit.append(name)
-# #     score = 0
-# #     comb = list(itertools.permutations(freq_reddit, 2))
-# #     weight_sum = 0
-# #     if len(comb) > 0:
-# #         for (name1, name2) in comb:
-# #             topic_id1 = obj['topicvecs'][name1]
-# #             topic_id2 = obj['topicvecs'][name2]
-# #             weight = math.sqrt(obj['contributions'][name1] * obj['contributions'][name2])
-# #             score += weight * (1 - h[topic_id1, topic_id2]) # 1 - Hellinger Distance = simularity
-# #             weight_sum += weight
-# #         score = score / weight_sum
-# #         print author, obj, score
-
-
-# # # # print_general_subreddit_topic()
+# for author, obj in author_topics.items():
+#     """
+#     First filter by weight cutoff T = 4, remove subreddit contributions less than T.
+#     """
+#     freq_reddit = []
+#     for name in obj['contributions'].iterkeys():
+#         if obj['contributions'][name] >= 4:
+#             freq_reddit.append(name)
+#     score = 0
+#     comb = list(itertools.permutations(freq_reddit, 2))
+#     weight_sum = 0
+#     if len(comb) > 0:
+#         for (name1, name2) in comb:
+#             topic_id1 = obj['topicvecs'][name1]
+#             topic_id2 = obj['topicvecs'][name2]
+#             weight = math.sqrt(obj['contributions'][name1] * obj['contributions'][name2])
+#             score += weight * (1 - h[topic_id1, topic_id2]) # 1 - Hellinger Distance = simularity
+#             weight_sum += weight
+#         score = score / weight_sum
+#         print author, obj, score
 
 
-# # # # # Clean the matrix; And slice with the fist 50 non empty subreddit.
-# # # # from sklearn.manifold import TSNE
-# # # # import matplotlib.pyplot as plt
-# # # # import matplotlib.cm as cm
-# # # # import numpy as np
-# # # # from adjustText import adjust_text
-# # # # import math
-# # # # def plot():
-# # # #     counter = 0
-# # # #     new_labels = []
-# # # #     new_rc_tvec = []
-# # # #     new_comment_counter = []
-# # # #     for label, t_dist, cc in zip(subreddits, rc_tvec, commentCounters):
-# # # #         if len(t_dist) == 0:
-# # # #             continue
-# # # #         else:
-# # # #             counter += 1
-# # # #             if counter < 1000:
-# # # #                 new_labels.append(label)
-# # # #                 new_rc_tvec.append(t_dist)
-# # # #                 new_comment_counter.append(cc)
+# # print_general_subreddit_topic()
 
 
+# Clean the matrix; And slice with the fist 50 non empty subreddit.
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+from adjustText import adjust_text
+import math
+def plot():
+    counter = 0
+    new_labels = []
+    new_rc_tvec = []
+    new_comment_counter = []
+    for label, t_dist, cc in zip(subreddits, rc_tvec, commentCounters):
+        if len(t_dist) == 0:
+            continue
+        else:
+            counter += 1
+            if counter < 1000:
+                new_labels.append(label)
+                new_rc_tvec.append(t_dist)
+                new_comment_counter.append(cc)
 
 
 
-# # # #     colors = cm.rainbow(np.linspace(0, 1, len(new_labels)))
 
-# # # #     tsne_results = TSNE(n_components=2, perplexity=40, verbose=2).fit_transform(rc_tvec)
 
-# # # #     def tsne_plot(labels, tokens, cc):
+    colors = cm.rainbow(np.linspace(0, 1, len(new_labels)))
+
+    tsne_results = TSNE(n_components=2, perplexity=40, verbose=2).fit_transform(rc_tvec)
+
+    def tsne_plot(labels, tokens, cc):
         
-# # # #         labels = labels
-# # # #         tokens = tokens
+        labels = labels
+        tokens = tokens
         
-# # # #         tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
-# # # #         new_values = tsne_model.fit_transform(tokens)
+        tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
+        new_values = tsne_model.fit_transform(tokens)
 
-# # # #         x = []
-# # # #         y = []
-# # # #         for value in new_values:
-# # # #             x.append(value[0])
-# # # #             y.append(value[1])
+        x = []
+        y = []
+        for value in new_values:
+            x.append(value[0])
+            y.append(value[1])
             
-# # # #         plt.figure(figsize=(16, 16)) 
-# # # #         # fig, ax = plt.subplots() # note we must use plt.subplots, not plt.subplot
+        plt.figure(figsize=(16, 16)) 
+        # fig, ax = plt.subplots() # note we must use plt.subplots, not plt.subplot
 
-# # # #         for i in range(len(x)):
-# # # #             sct = plt.scatter(x[i],y[i], color=colors[i], s=(float(cc[i]) / 90 ), linewidths=2, edgecolor='w')
-# # # #             sct.set_alpha(0.75)
-# # # #             if float(cc[i]) > float(cc[200]):
-# # # #                 plt.annotate(labels[i],
-# # # #                             xy=(x[i], y[i]),
-# # # #                             xytext=(5, 2),
-# # # #                             textcoords='offset points',
-# # # #                             ha='right',
-# # # #                             va='bottom')
-# # # #             # Draw cycles.
-# # # #             # cycle = plt.Circle((x[i],y[i]), math.log1p(float(cc[i])), fill=False)
-# # # #             # ax.add_artist(cycle)
+        for i in range(len(x)):
+            sct = plt.scatter(x[i],y[i], color=colors[i], s=(float(cc[i]) / 90 ), linewidths=2, edgecolor='w')
+            sct.set_alpha(0.75)
+            if float(cc[i]) > float(cc[200]):
+                plt.annotate(labels[i],
+                            xy=(x[i], y[i]),
+                            xytext=(5, 2),
+                            textcoords='offset points',
+                            ha='right',
+                            va='bottom')
+            # Draw cycles.
+            # cycle = plt.Circle((x[i],y[i]), math.log1p(float(cc[i])), fill=False)
+            # ax.add_artist(cycle)
 
-# # # #         # texts = []
-# # # #         # for m, n, p in zip(x, y, labels):
-# # # #         #     texts.append(plt.text(m, n, p))
-# # # #         # adjust_text(texts, only_move={'points':'y', 'text':'y'})
+        # texts = []
+        # for m, n, p in zip(x, y, labels):
+        #     texts.append(plt.text(m, n, p))
+        # adjust_text(texts, only_move={'points':'y', 'text':'y'})
 
         
-# # # #         plt.show()
+        plt.show()
 
-# # # #     tsne_plot(new_labels, new_rc_tvec, new_comment_counter)
+    tsne_plot(new_labels, new_rc_tvec, new_comment_counter)
 
-# # # #     # print(subreddits)
-# # # #     # pprint(ldamodel.print_topics(num_topics=100, num_words=5))
-# # # #     # pprint(ldamodel.print_topics(num_topics=3, num_words=3))
+    # print(subreddits)
+    # pprint(ldamodel.print_topics(num_topics=100, num_words=5))
+    # pprint(ldamodel.print_topics(num_topics=3, num_words=3))
 
-# # # # plot()
+plot()

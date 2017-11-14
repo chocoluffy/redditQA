5e9bbeb6348bbe3a38a592682409e7b8bd3c3e83
chocoluffy <luffy.yu@mail.utoronto.ca>
2017-11-13 21:12:17 -0500

    Fix bug

--- 6-Word2Vec/author_csv_31G.py
+++ 6-Word2Vec/author_csv_31G.py
@@ -5,4 +5,4 @@ VERSION_PATH = './models/no_tfidf_topic_100_31G_data'
 AUTHOR_CSV = os.path.join(VERSION_PATH, 'each_author_topic_comment.csv')
 
 author_stats_large = return_author_stats(VERSION_PATH)
-write_dict_data_to_csv_file(AUTHOR_CSV, author_stats_large):
+write_dict_data_to_csv_file(AUTHOR_CSV, author_stats_large)

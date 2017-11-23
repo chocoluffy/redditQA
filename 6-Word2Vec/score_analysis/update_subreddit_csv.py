74c419f710a61ea9e9e53b7f439f05630d420768
chocoluffy <luffy.yu@mail.utoronto.ca>
2017-11-23 10:26:16 -0500

    Make plot on total counts instead of total ups

--- 6-Word2Vec/score_analysis/update_subreddit_csv.py
+++ 6-Word2Vec/score_analysis/update_subreddit_csv.py
@@ -13,16 +13,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Gloabl Configuration
-VERSION_PATH = './models/no_tfidf_topic_100_31G_data'
+"""
+Global configuration.
+"""
+IS_LOCAL = True # test 8G data on local machine. test 31G data on remote server.
 
-DICTIONARY_PATH = os.path.join(VERSION_PATH, 'dictionary.dict')
-CORPUS_PATH = os.path.join(VERSION_PATH, 'corpus.mm')
-CORPUS_TFIDF_PATH = os.path.join(VERSION_PATH, 'corpus-tfidf.mm')
-LDA_PATH = os.path.join(VERSION_PATH, 'model.lda')
-TOP_COMMENTS = os.path.join(VERSION_PATH, '31G_top25subreddit_top6kcomments.pkl')
-AUTHOR_STATS = os.path.join(VERSION_PATH, 'each_author_topic_comments_with_count.pkl')
-SUBREDDIT_CSV = os.path.join(VERSION_PATH, '31G_subreddit.csv')
+if not IS_LOCAL:
+    VERSION_PATH = './models/no_tfidf_topic_100_31G_data'
+
+    DICTIONARY_PATH = os.path.join(VERSION_PATH, 'dictionary.dict')
+    CORPUS_PATH = os.path.join(VERSION_PATH, 'corpus.mm')
+    CORPUS_TFIDF_PATH = os.path.join(VERSION_PATH, 'corpus-tfidf.mm')
+    LDA_PATH = os.path.join(VERSION_PATH, 'model.lda')
+    TOP_COMMENTS = os.path.join(VERSION_PATH, '31G_top25subreddit_top6kcomments.pkl')
+    AUTHOR_STATS = os.path.join(VERSION_PATH, 'each_author_topic_comments_with_count.pkl')
+    SUBREDDIT_CSV = os.path.join(VERSION_PATH, '31G_subreddit.csv')
+else:
+    VERSION_PATH = './models/no_tfidf_topic_100_8G_data'
+
+    DICTIONARY_PATH = os.path.join(VERSION_PATH, 'dictionary.dict')
+    CORPUS_PATH = os.path.join(VERSION_PATH, 'corpus.mm')
+    CORPUS_TFIDF_PATH = os.path.join(VERSION_PATH, 'corpus-tfidf.mm')
+    LDA_PATH = os.path.join(VERSION_PATH, 'model.lda')
+    TOP_COMMENTS = os.path.join(VERSION_PATH, '8G_top010subreddit_top2kcomments.pkl')
+    AUTHOR_STATS = os.path.join(VERSION_PATH, 'each_author_topic_comments.pkl')
+    SUBREDDIT_CSV = os.path.join(VERSION_PATH, '8G_subreddit.csv')
+
+
 
 
 """
@@ -41,13 +58,13 @@ each user(row) has fields ["name", "dom_topics", "dom_topic_str", "score", "mapp
 scores = []
 
 # Add field "dom_topic_str" to the author_stats.
-topic2str = defaultdict(str)
-for i in range(100):
-    topic2str[i] = ' + '.join(map(lambda x: x[0], ldamodel.show_topic(i, topn=4)))
+# topic2str = defaultdict(str)
+# for i in range(100):
+#     topic2str[i] = ' + '.join(map(lambda x: x[0], ldamodel.show_topic(i, topn=4)))
 
 for name, obj in author_stats.iteritems():
-    topic_str = ["({0}, {1})".format(topic2str[tup[0]], tup[1]) for tup in obj['dom_topics']]
-    author_stats[name]['dom_topics_str'] = topic_str
+    # topic_str = ["({0}, {1})".format(topic2str[tup[0]], tup[1]) for tup in obj['dom_topics']]
+    # author_stats[name]['dom_topics_str'] = topic_str
     scores.append(obj['score'])
 
 scale = interp1d([min(scores), max(scores)],[1,100])
@@ -91,7 +108,8 @@ def construct_reddit(if_by_overlap = False):
     reddit = defaultdict(lambda:defaultdict(list))
     for author_name, obj in author_stats.iteritems():
         for reddit_name, ups in obj['contributions'].iteritems():
-            reddit[reddit_name]['involvements'].append((author_name, ups))
+            author_this_subreddit_count = obj['contributions_by_count'][reddit_name]
+            reddit[reddit_name]['involvements'].append((author_name, ups, author_this_subreddit_count))
             if if_by_overlap:
                 reddit[reddit_name]['scores'].append(obj['mapped_score_by_overlap'])
             else:
@@ -106,9 +124,9 @@ def construct_reddit(if_by_overlap = False):
             for index, value in enumerate(topic_vec):
                 if value > 0.1: # use 0.1 as topic cutoff
                     filtered_topics.append((index, value))
-            topic_str = map(lambda x: (topic2str[x[0]], x[1]), filtered_topics)
-            topic_str = sorted(topic_str, key=lambda tup: tup[1], reverse=True)
-            reddit[reddit_name]['dom_topic_str'] = topic_str
+            # topic_str = map(lambda x: (topic2str[x[0]], x[1]), filtered_topics)
+            # topic_str = sorted(topic_str, key=lambda tup: tup[1], reverse=True)
+            # reddit[reddit_name]['dom_topic_str'] = topic_str
             reddit[reddit_name]['dom_topic'] = filtered_topics
             reddit[reddit_name]['comments'] = reddit_2_topic[reddit_name]['doc']
     return reddit
@@ -118,7 +136,7 @@ def subreddit_elite_score(reddit, if_by_overlap = False):
     ELITE_PERCENTAGE = 0.05
     for reddit_name, obj in reddit.iteritems():
         involvements = obj['involvements']
-        involvements = sorted(involvements, key=lambda tup: tup[1], reverse=True)
+        involvements = sorted(involvements, key=lambda tup: tup[2], reverse=True) # sort by [1]: total ups or [2]: total counts.
         counter = 0
         total = len(involvements) * ELITE_PERCENTAGE
         score_lst = []
@@ -179,77 +197,76 @@ reddit = construct_reddit(IF_BY_OVERLAP)
 reddit = subreddit_elite_score(reddit, IF_BY_OVERLAP)
 
 
-subreddit_to_authors_distribution(reddit)
+# subreddit_to_authors_distribution(reddit)
 
 """
 Below is generating the plot for score distribution. 
 """
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+import math
+from adjustText import adjust_text
 
-# import matplotlib.pyplot as plt
-# import matplotlib.cm as cm
-# import numpy as np
-# import math
-# from adjustText import adjust_text
-
-# def plot(reddit_data, adjust = False):
-#     """
-#     x: average generalist/specialist score.
-#     y: average elite's generalist/specialist score.
-#     radius: total authors involved.
-#     """
-#     labels = []
-#     x = []
-#     y = []
-#     r = []
+def plot(reddit_data, adjust = False):
+    """
+    x: average generalist/specialist score.
+    y: average elite's generalist/specialist score.
+    radius: total authors involved.
+    """
+    labels = []
+    x = []
+    y = []
+    r = []
 
 
-#     # pprint(reddit)
-#     for name, obj in reddit_data.iteritems():
-#         if len(obj['involvements']) > 10: # only pick active subreddits.
-#             labels.append(name)
-#             x.append(sum(obj['scores']) / len(obj['scores']))
-#             y.append(obj['elite_scores'])
-#             r.append(len(obj['involvements']))
+    # pprint(reddit)
+    for name, obj in reddit_data.iteritems():
+        if len(obj['involvements']) > 10: # only pick active subreddits.
+            labels.append(name)
+            x.append(sum(obj['scores']) / len(obj['scores']))
+            y.append(obj['elite_scores'])
+            r.append(len(obj['involvements']))
     
-#     colors = cm.rainbow(np.linspace(0, 1, len(labels)))
+    colors = cm.rainbow(np.linspace(0, 1, len(labels)))
                    
-#     fig = plt.figure(figsize=(16, 16)) 
-#     axes = plt.gca()
-#     axes.set_xlim([1,100])
-#     axes.set_ylim([1,100])
-#     ax = fig.add_subplot(111)
-#     fig.subplots_adjust(top=0.85)
-#     ax.set_xlabel('average G/S score')
-#     ax.set_ylabel('average elite G/S score')
-#     ax.plot(ax.get_xlim(), ax.get_ylim(), ls="--", c=".3")
-#     plt.title("common VS elites G/S score")
+    fig = plt.figure(figsize=(16, 16)) 
+    axes = plt.gca()
+    axes.set_xlim([1,100])
+    axes.set_ylim([1,100])
+    ax = fig.add_subplot(111)
+    fig.subplots_adjust(top=0.85)
+    ax.set_xlabel('average G/S score')
+    ax.set_ylabel('average elite G/S score')
+    ax.plot(ax.get_xlim(), ax.get_ylim(), ls="--", c=".3")
+    plt.title("common VS elites G/S score")
 
-#     # to dynamic adjust texts labels.
-#     texts = []
-#     annotate_x = []
-#     annotate_y = []
-#     for xx, yy, ll in zip(x, y, labels):
-#         if abs(yy - xx) > 25:
-#             texts.append(ax.text(xx, yy, ll))
-#             annotate_x.append(xx)
-#             annotate_y.append(yy)
+    # to dynamic adjust texts labels.
+    texts = []
+    annotate_x = []
+    annotate_y = []
+    for xx, yy, ll in zip(x, y, labels):
+        if abs(yy - xx) > 25:
+            texts.append(ax.text(xx, yy, ll))
+            annotate_x.append(xx)
+            annotate_y.append(yy)
 
-#     if adjust:
-#         for i in range(len(x)):
-#             sct = plt.scatter(x[i],y[i], color=colors[i], s=(float(r[i]) * 3), linewidths=2, edgecolor='w')
-#             sct.set_alpha(0.75)
-#         adjust_text(texts, annotate_x, annotate_y, arrowprops=dict(arrowstyle="-", color='k', lw=0.5))
-#     else:
-#         for i in range(len(x)):
-#             sct = plt.scatter(x[i],y[i], color=colors[i], s=(float(r[i]) * 3), linewidths=2, edgecolor='w')
-#             sct.set_alpha(0.75)
-#             if abs(y[i] - x[i]) > 25:
-#                 plt.annotate(labels[i],
-#                             xy=(x[i], y[i]),
-#                             xytext=(5, 2),
-#                             textcoords='offset points',
-#                             ha='right',
-#                             va='bottom')
-#     plt.show()
+    if adjust:
+        for i in range(len(x)):
+            sct = plt.scatter(x[i],y[i], color=colors[i], s=(float(r[i]) * 3), linewidths=2, edgecolor='w')
+            sct.set_alpha(0.75)
+        adjust_text(texts, annotate_x, annotate_y, arrowprops=dict(arrowstyle="-", color='k', lw=0.5))
+    else:
+        for i in range(len(x)):
+            sct = plt.scatter(x[i],y[i], color=colors[i], s=(float(r[i]) * 3), linewidths=2, edgecolor='w')
+            sct.set_alpha(0.75)
+            if abs(y[i] - x[i]) > 25:
+                plt.annotate(labels[i],
+                            xy=(x[i], y[i]),
+                            xytext=(5, 2),
+                            textcoords='offset points',
+                            ha='right',
+                            va='bottom')
+    plt.show()
 
-# plot(reddit, adjust = True)
+plot(reddit, adjust = True)

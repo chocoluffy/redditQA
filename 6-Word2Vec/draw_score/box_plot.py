30420a0a2f82e8ff7315c11be7dd2719ddc4d9bc
chocoluffy <luffy.yu@mail.utoronto.ca>
2017-12-01 11:04:34 -0500

    Add boxplot

--- 6-Word2Vec/draw_score/box_plot.py
+++ 6-Word2Vec/draw_score/box_plot.py
@@ -120,73 +120,40 @@ else:
 """
 Make plot.
 """
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import numpy as np
-import math
-from adjustText import adjust_text
+import numpy as np 
+import matplotlib as mpl 
+
+## agg backend is used to create plot as a .png file
+mpl.use('agg')
+
+import matplotlib.pyplot as plt 
+
+def init_list_of_objects(size):
+    list_of_objects = list()
+    for i in range(0,size):
+        list_of_objects.append( list() ) #different object reference each time
+    return list_of_objects
+
 
 def plot(reddit_score_to_comment_count, adjust = False):
-    """
-    x: average generalist/specialist score.
-    y: average elite's generalist/specialist score.
-    radius: total authors involved.
-    """
-    labels = []
-    x = []
-    y = []
-    r = []
 
+    data_collections = init_list_of_objects(10)
 
     # pprint(reddit)
     for name, obj in reddit_score_to_comment_count.iteritems():
-        if obj['valid_comment_count'] > 500: # only pick active subreddits.
-            labels.append(name)
-            x.append(obj['valid_comment_count'])
-            y.append(obj['0.1'])
-            r.append(obj['comments_count'])
-    
-    colors = cm.rainbow(np.linspace(0, 1, len(labels)))
+        bucket_id = round(obj['1'] / 5)
+        data_collections = data_collections[bucket_id].append(obj['0.1'])
 
-    print x, y, r
-                
-    fig = plt.figure(figsize=(16, 16)) 
-    # axes = plt.gca()
-    # axes.set_xlim([1,100])
-    # axes.set_ylim([1,100])
+    # Create a figure instance
+    fig = plt.figure(1, figsize=(9, 6))
+
+    # Create an axes instance
     ax = fig.add_subplot(111)
-    fig.subplots_adjust(top=0.85)
-    ax.set_xlabel('#comments')
-    ax.set_ylabel('average most successful author G/S score')
-    # ax.plot(ax.get_xlim(), ax.get_ylim(), ls="--", c=".3")
-    plt.title("successful author - comments count")
 
-    # to dynamic adjust texts labels.
-    texts = []
-    annotate_x = []
-    annotate_y = []
-    for xx, yy, ll in zip(x, y, labels):
-        if xx > 1000:
-            texts.append(ax.text(xx, yy, ll))
-            annotate_x.append(xx)
-            annotate_y.append(yy)
+    # Create the boxplot
+    bp = ax.boxplot(data_collections)
 
-    if adjust:
-        for i in range(len(x)):
-            sct = plt.scatter(x[i],y[i], color=colors[i], s=(math.log(float(r[i])) * 10), linewidths=2, edgecolor='w')
-            sct.set_alpha(0.75)
-        adjust_text(texts, annotate_x, annotate_y, arrowprops=dict(arrowstyle="-", color='k', lw=0.5))
-    else:
-        for i in range(len(x)):
-            sct = plt.scatter(x[i],y[i], color=colors[i], s=(float(r[i]) * 0.1), linewidths=2, edgecolor='w')
-            sct.set_alpha(0.75)
-            if abs(y[i] - x[i]) > 25:
-                plt.annotate(labels[i],
-                            xy=(x[i], y[i]),
-                            xytext=(5, 2),
-                            textcoords='offset points',
-                            ha='right',
-                            va='bottom')
-    plt.show()
+    # Save the figure
+    fig.savefig('fig1.png', bbox_inches='tight')
 
 plot(reddit_score_to_comment_count, adjust = True)

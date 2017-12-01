97677cfe58a18a41eca2e3f9901e5d409e29606b
chocoluffy <luffy.yu@mail.utoronto.ca>
2017-12-01 09:30:21 -0500

    Draw graph on score to comment count

--- 6-Word2Vec/draw_score/score_to_comment_count.py
+++ 6-Word2Vec/draw_score/score_to_comment_count.py
@@ -6,6 +6,7 @@ import pickle
 import os.path
 import csv
 from collections import defaultdict
+import math
 
 VERSION_PATH = './models/no_tfidf_topic_100_8G_data'
 REDDIT_ALL = os.path.join(VERSION_PATH, '31G_reddit_all.pkl')
@@ -13,6 +14,7 @@ AUTHOR_STATS = os.path.join(VERSION_PATH, '31G_complete_author_stats.pkl') # eac
 TOP_COMMENTS = os.path.join(VERSION_PATH, '8G_top010subreddit_top2kcomments_with_author.pkl')
 NEW_TOP_COMMENTS = os.path.join(VERSION_PATH, '8G_top010subreddit_top2kcomments_with_author_and_score.pkl')
 TOP_SUCCESSFUL_AUTHOR_SCORE_ANALYSIS = os.path.join(VERSION_PATH, '8G_most_successful_author_score_analysis.csv')
+REDDIT_SCORE_TO_COMMENT = os.path.join(VERSION_PATH, '8G_suddreddit_score_to_comments.pkl')
 
 TOP_K_SUCCESSFUL_AUTHOR = [0.01, 0.05, 0.1, 0.2, 0.5, 1] # check score from those top successful authors from each subreddit.
 KEYWORD = 'lda' # 'lda'; 'overlap'; 'entropy'
@@ -74,105 +76,115 @@ csv_file_path = TOP_SUCCESSFUL_AUTHOR_SCORE_ANALYSIS
 csv_file = open(csv_file_path, 'wb')
 writer = csv.writer(csv_file, dialect='excel')
 
-headers = ['name', 'comments_count'].extend(map(lambda x: str(x), TOP_K_SUCCESSFUL_AUTHOR))
+headers = ['name', 'comments_count', 'valid_comment_count']
+headers.extend(map(lambda x: str(x), TOP_K_SUCCESSFUL_AUTHOR))
 writer.writerow(headers)
 
-for reddit_name, obj in new_top_comments.items():
-    line = []
-    for field in headers:
-        if field == 'name':
-            line.append(reddit_name)
-        elif field == 'comments_count':
-            line.append(obj['length'])
-        else: # a percentage
-            ratio = float(field)
-            id = round(len(obj['top_comments']) * ratio)
-            counter = 0
-            score_lst = []
-            while counter < id:
-                score_lst.append(obj['top_comments'][counter])
-                counter += 1
-            score_average = sum(score_lst) / len(score_lst)
-            line.append(score_average)
 
-    writer.writerow(line)
-    
-csv_file.close()
-
-
-
+if not os.path.exists(REDDIT_SCORE_TO_COMMENT):
+    reddit_score_to_comment_count = defaultdict(dict)
+    for reddit_name, obj in new_top_comments.items():
+        line = []
+        for field in headers:
+            if field == 'name':
+                line.append(reddit_name)
+            elif field == 'comments_count':
+                line.append(obj['length'])
+                reddit_score_to_comment_count[reddit_name]['comments_count'] = obj['length']
+            elif field == 'valid_comment_count':
+                line.append(len(obj['top_comments']))
+                reddit_score_to_comment_count[reddit_name]['valid_comment_count'] = len(obj['top_comments'])
+            else: # a percentage
+                ratio = float(field)
+                id = math.ceil(len(obj['top_comments']) * ratio)
+                counter = 0
+                score_lst = []
+                while counter < id:
+                    score_lst.append(obj['top_comments'][counter][2]) # add the mapped_score_overlap
+                    counter += 1
+                if len(score_lst) > 0:
+                    score_average = sum(score_lst) / len(score_lst)
+                else:
+                    score_average = []
+                line.append(score_average)
+                reddit_score_to_comment_count[reddit_name][field] = score_average
+        writer.writerow(line)
+        
+    csv_file.close()
+    pickle.dump(reddit_score_to_comment_count, open(REDDIT_SCORE_TO_COMMENT, 'wb'))
+else: 
+    reddit_score_to_comment_count = pickle.load(open(REDDIT_SCORE_TO_COMMENT, 'rb'))
 
         
 
+"""
+Make plot.
+"""
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+import math
+from adjustText import adjust_text
 
-# """
-# Make plot.
-# """
-# import matplotlib.pyplot as plt
-# import matplotlib.cm as cm
-# import numpy as np
-# import math
-# from adjustText import adjust_text
-
-# def plot(reddit_data, adjust = False):
-#     """
-#     x: average generalist/specialist score.
-#     y: average elite's generalist/specialist score.
-#     radius: total authors involved.
-#     """
-#     labels = []
-#     x = []
-#     y = []
-#     r = []
+def plot(reddit_data, adjust = False):
+    """
+    x: average generalist/specialist score.
+    y: average elite's generalist/specialist score.
+    radius: total authors involved.
+    """
+    labels = []
+    x = []
+    y = []
+    r = []
 
 
-#     # pprint(reddit)
-#     for name, obj in reddit_data.iteritems():
-#         if len(obj['involvements']) > 500: # only pick active subreddits.
-#             labels.append(name)
-#             x.append(sum(obj[common_score]) / len(obj[common_score]))
-#             y.append(obj[elite_score])
-#             r.append(len(obj['involvements']))
+    # pprint(reddit)
+    for name, obj in reddit_score_to_comment_count.iteritems():
+        if len(obj['valid_comment_count']) > 500: # only pick active subreddits.
+            labels.append(name)
+            x.append(obj['valid_comment_count'])
+            y.append(obj['0.1'])
+            r.append(len(obj['comments_count']))
     
-#     colors = cm.rainbow(np.linspace(0, 1, len(labels)))
+    colors = cm.rainbow(np.linspace(0, 1, len(labels)))
                 
-#     fig = plt.figure(figsize=(16, 16)) 
-#     axes = plt.gca()
-#     axes.set_xlim([1,100])
-#     axes.set_ylim([1,100])
-#     ax = fig.add_subplot(111)
-#     fig.subplots_adjust(top=0.85)
-#     ax.set_xlabel('average G/S score')
-#     ax.set_ylabel('average elite G/S score')
-#     ax.plot(ax.get_xlim(), ax.get_ylim(), ls="--", c=".3")
-#     plt.title("common VS elites G/S score")
+    fig = plt.figure(figsize=(16, 16)) 
+    axes = plt.gca()
+    axes.set_xlim([1,100])
+    axes.set_ylim([1,100])
+    ax = fig.add_subplot(111)
+    fig.subplots_adjust(top=0.85)
+    ax.set_xlabel('#comments')
+    ax.set_ylabel('average most successful author G/S score')
+    ax.plot(ax.get_xlim(), ax.get_ylim(), ls="--", c=".3")
+    plt.title("successful author - comments count")
 
-#     # to dynamic adjust texts labels.
-#     texts = []
-#     annotate_x = []
-#     annotate_y = []
-#     for xx, yy, ll in zip(x, y, labels):
-#         if abs(yy - xx) > 10:
-#             texts.append(ax.text(xx, yy, ll))
-#             annotate_x.append(xx)
-#             annotate_y.append(yy)
+    # to dynamic adjust texts labels.
+    texts = []
+    annotate_x = []
+    annotate_y = []
+    for xx, yy, ll in zip(x, y, labels):
+        if xx > 1000:
+            texts.append(ax.text(xx, yy, ll))
+            annotate_x.append(xx)
+            annotate_y.append(yy)
 
-#     if adjust:
-#         for i in range(len(x)):
-#             sct = plt.scatter(x[i],y[i], color=colors[i], s=(float(r[i]) * 0.1), linewidths=2, edgecolor='w')
-#             sct.set_alpha(0.75)
-#         adjust_text(texts, annotate_x, annotate_y, arrowprops=dict(arrowstyle="-", color='k', lw=0.5))
-#     else:
-#         for i in range(len(x)):
-#             sct = plt.scatter(x[i],y[i], color=colors[i], s=(float(r[i]) * 0.1), linewidths=2, edgecolor='w')
-#             sct.set_alpha(0.75)
-#             if abs(y[i] - x[i]) > 25:
-#                 plt.annotate(labels[i],
-#                             xy=(x[i], y[i]),
-#                             xytext=(5, 2),
-#                             textcoords='offset points',
-#                             ha='right',
-#                             va='bottom')
-#     plt.show()
+    if adjust:
+        for i in range(len(x)):
+            sct = plt.scatter(x[i],y[i], color=colors[i], s=(math.log(float(r[i]))), linewidths=2, edgecolor='w')
+            sct.set_alpha(0.75)
+        adjust_text(texts, annotate_x, annotate_y, arrowprops=dict(arrowstyle="-", color='k', lw=0.5))
+    else:
+        for i in range(len(x)):
+            sct = plt.scatter(x[i],y[i], color=colors[i], s=(float(r[i]) * 0.1), linewidths=2, edgecolor='w')
+            sct.set_alpha(0.75)
+            if abs(y[i] - x[i]) > 25:
+                plt.annotate(labels[i],
+                            xy=(x[i], y[i]),
+                            xytext=(5, 2),
+                            textcoords='offset points',
+                            ha='right',
+                            va='bottom')
+    plt.show()
 
-# plot(reddit, adjust = True)
+plot(reddit, adjust = True)

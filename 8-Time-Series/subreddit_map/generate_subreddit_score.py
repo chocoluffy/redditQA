ca779fbaefc5ab8f484fee0a26742b338b551920
chocoluffy <luffy.yu@mail.utoronto.ca>
2017-12-09 16:29:40 -0500

    Add generate subreddit score file

---
+++ 8-Time-Series/subreddit_map/generate_subreddit_score.py
@@ -0,0 +1,67 @@
+"""
+Given subreddit stats on its contributions. 
+
+- pickle files from ./models/subreddit_vector_201x are trained from year's data.
+
+- procedure:
+    - Aggregate by author, only need its contribution count, not votes. For top authors(more complete, the better).
+    - calculate G/S score for each author by subreddit embeddings from LSI.
+    - Aggregate by subreddit, use involvement, then calculate the G/S score for each subreddit. 
+    - Make plot.
+"""
+from __future__ import division
+import operator
+import json
+from pprint import pprint
+from pymongo import MongoClient
+import pickle
+from collections import defaultdict
+import os.path
+import re
+import itertools
+from scipy.interpolate import interp1d
+import math
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+VERSION_PATH = './models/201301'
+AUTHOR_STATS_WITH_CONTRIBUTION_COUNT = os.path.join(VERSION_PATH, 'author_comments_stats_with_score.pkl')
+SUBREDDIT_STATS = os.path.join(VERSION_PATH, 'reddit_stats.pkl')
+author_stats = pickle.load(open(AUTHOR_STATS_WITH_CONTRIBUTION_COUNT, 'rb'))
+
+def append_item_to_list(dictionary, field, item):
+    if field in dictionary:
+        dictionary[field].append(item)
+    else:
+        dictionary[field] = [item]
+
+"""
+reddit.name.involvements is a list of tuples: (author name, total ups, total comment counts, author score)
+
+involvements sorted by total ups, meaning most succesfully author.
+"""
+reddit = defaultdict(dict)
+ELITE_PERCENTAGE = 0.05
+
+for author_name, obj in author_stats.iteritems():
+    for reddit_name, ups in obj['contributions_by_count']: # currently obj['contributions'] is a list.
+        author_this_subreddit_count = obj['contributions_by_count'][reddit_name]
+        append_item_to_list(reddit[reddit_name], 'involvements', (author_name, ups, author_this_subreddit_count, obj['mapped_score_by_overlap']))
+        reddit[reddit_name]['name'] = reddit_name
+
+        involvements = reddit[reddit_name]['involvements']
+        involvements = sorted(involvements, key=lambda tup: tup[1], reverse=True) # sort by [1]: total ups or [2]: total counts.
+        counter = 0
+        total = len(involvements) * ELITE_PERCENTAGE
+        elite_score_lst_overlap = []
+        while counter < total:
+            elite_score_lst_overlap.append(involvements[counter][3])
+            counter += 1
+        all_score_lst_overlap = map(lambda x: x[3], involvements)
+        aver_elite_overlap = sum(elite_score_lst_overlap) / len(elite_score_lst_overlap)
+        aver_all_overlap = sum(all_score_lst_overlap) / len(all_score_lst_overlap)
+        reddit[reddit_name]['elite_scores_overlap'] = aver_elite_overlap
+        reddit[reddit_name]['all_scores_overlap'] = aver_all_overlap
+
+pickle.dump(reddit, open(SUBREDDIT_STATS, 'wb'))
+print("saved stats for each subreddit...")

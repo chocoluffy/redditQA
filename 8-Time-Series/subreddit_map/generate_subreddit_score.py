42a612b7bd20991343564fe8fa11cc010392d2c2
chocoluffy <luffy.yu@mail.utoronto.ca>
2017-12-09 17:50:33 -0500

    Fix bug of wrong iteration

--- 8-Time-Series/subreddit_map/generate_subreddit_score.py
+++ 8-Time-Series/subreddit_map/generate_subreddit_score.py
@@ -46,31 +46,33 @@ ELITE_PERCENTAGE = 0.05
 author_count = 0
 for author_name, obj in author_stats.iteritems():
     author_count += 1
-    print "processing no.", author_count
+    if author_count % 200 == 0:
+        print "processing no.", author_count
     for reddit_name, ups in obj['contributions'].iteritems(): # currently obj['contributions'] is a list.
         author_this_subreddit_count = obj['contributions_by_count'][reddit_name]
         append_item_to_list(reddit[reddit_name], 'involvements', (author_name, ups, author_this_subreddit_count, obj['mapped_score_by_overlap']))
         reddit[reddit_name]['name'] = reddit_name
 
-        involvements = reddit[reddit_name]['involvements']
-        involvements = sorted(involvements, key=lambda tup: tup[1], reverse=True) # sort by [1]: total ups or [2]: total counts.
-        counter = 0
-        total = len(involvements) * ELITE_PERCENTAGE
-        elite_score_lst_overlap = []
-        while counter < total:
-            elite_score_lst_overlap.append(involvements[counter][3])
-            counter += 1
-        all_score_lst_overlap = map(lambda x: x[3], involvements)
-        if len(elite_score_lst_overlap) > 0:
-            aver_elite_overlap = sum(elite_score_lst_overlap) / len(elite_score_lst_overlap)
-        else: 
-            aver_elite_overlap = -1
-        if len(all_score_lst_overlap) > 0:
-            aver_all_overlap = sum(all_score_lst_overlap) / len(all_score_lst_overlap)
-        else:
-            aver_all_overlap = -1
-        reddit[reddit_name]['elite_scores_overlap'] = aver_elite_overlap
-        reddit[reddit_name]['all_scores_overlap'] = aver_all_overlap
+for reddit_name, obj in reddit.iteritems():
+    involvements = obj['involvements']
+    involvements = sorted(involvements, key=lambda tup: tup[1], reverse=True) # sort by [1]: total ups or [2]: total counts.
+    counter = 0
+    total = len(involvements) * ELITE_PERCENTAGE
+    elite_score_lst_overlap = []
+    while counter < total:
+        elite_score_lst_overlap.append(involvements[counter][3])
+        counter += 1
+    all_score_lst_overlap = map(lambda x: x[3], involvements)
+    if len(elite_score_lst_overlap) > 0:
+        aver_elite_overlap = sum(elite_score_lst_overlap) / len(elite_score_lst_overlap)
+    else: 
+        aver_elite_overlap = -1
+    if len(all_score_lst_overlap) > 0:
+        aver_all_overlap = sum(all_score_lst_overlap) / len(all_score_lst_overlap)
+    else:
+        aver_all_overlap = -1
+    reddit[reddit_name]['elite_scores_overlap'] = aver_elite_overlap
+    reddit[reddit_name]['all_scores_overlap'] = aver_all_overlap
 
 pickle.dump(reddit, open(SUBREDDIT_STATS, 'wb'))
 print("saved stats for each subreddit...")

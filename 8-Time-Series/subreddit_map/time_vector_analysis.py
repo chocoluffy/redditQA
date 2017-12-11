9d0606502dab793881cc8014ac36bcae168158b6
chocoluffy <luffy.yu@mail.utoronto.ca>
2017-12-10 21:34:37 -0500

    Save shared reddit stats

--- 8-Time-Series/subreddit_map/time_vector_analysis.py
+++ 8-Time-Series/subreddit_map/time_vector_analysis.py
@@ -16,7 +16,7 @@ contains fields:
 """
 import os.path
 import pickle
-import defaultdict
+from collections import defaultdict
 
 VERSION_PATH_2013 = './models/201301'
 SUBREDDIT_STATS_2013 = os.path.join(VERSION_PATH_2013, 'reddit_stats.pkl')
@@ -33,6 +33,8 @@ SUBREDDIT_STATS_2015 = os.path.join(VERSION_PATH_2015, 'reddit_stats.pkl')
 reddit_stats_2015 = pickle.load(open(SUBREDDIT_STATS_2015, 'rb'))
 print "loaded reddit stats at 201501"
 
+SHARED_REDDITS = os.path.join('./models', 'shared_reddits.pkl')
+
 subreddit_name_lst = map(lambda x: x.iterkeys(), [reddit_stats_2013, reddit_stats_2014, reddit_stats_2015])
 
 shared_name_lst = list(reduce(set.intersection, [set(year) for year in subreddit_name_lst ]))
@@ -43,12 +45,12 @@ for reddit_name in shared_name_lst:
     allscore_time_vec = []
     elitescore_time_vec = []
     for data in [reddit_stats_2013, reddit_stats_2014, reddit_stats_2015]:
-        involvements_time_vec.append(len(data['involvements']))
-        allscore_time_vec.append(data['all_scores_overlap'])
-        elitescore_time_vec.append(data['elite_scores_overlap'])
+        involvements_time_vec.append(len(data[reddit_name]['involvements']))
+        allscore_time_vec.append(data[reddit_name]['all_scores_overlap'])
+        elitescore_time_vec.append(data[reddit_name]['elite_scores_overlap'])
     shared_reddits[reddit_name]['involvement_vec'] = involvements_time_vec
     shared_reddits[reddit_name]['all_score_vec'] = allscore_time_vec
     shared_reddits[reddit_name]['elite_score_vec'] = elitescore_time_vec
 
-
+pickle.dump(shared_reddits, open(SHARED_REDDITS, 'wb'))
 
